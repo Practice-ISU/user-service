ab50dd4ba5f4b3525bc15b0320d9cc59b5e8fbc3
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-06-21 01:38:43 +0800

    entry point of service

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"log"
+	"net"
+	psql_conf "user-service/configs/postgre"
+	server "user-service/internal/adapters/api/grpc"
+	psql_stor "user-service/internal/adapters/db/postgre"
+	"user-service/internal/domain/service"
+	pkg_grpc "user-service/pkg/grpc"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	userStorage, err := psql_stor.NewUserStorage(psql_conf.GetDefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+	userSevice := service.NewUserService(userStorage)
+	userServer := server.NewUserServer(userSevice)
+
+	s := grpc.NewServer()
+	pkg_grpc.RegisterUserGrpcServiceServer(s, userServer)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.Serve(l)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
